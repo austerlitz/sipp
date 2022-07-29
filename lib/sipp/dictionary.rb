a77284a59b6c1ab9df552c8e39910cc920371f42
Max Buslaev <max@buslaev.net>
2022-07-29 23:43:50 +0300

    add dictionaries

--- lib/sipp/dictionary.rb
+++ lib/sipp/dictionary.rb
@@ -1,2 +1,80 @@
-module Dictionary
+module Sipp
+  module Dictionary
+    CATEGORY = {
+      'M' => 'Mini',
+      'N' => 'Mini Elite',
+      'E' => 'Economy',
+      'H' => 'Economy Elite',
+      'C' => 'Compact',
+      'D' => 'Compact Elite',
+      'I' => 'Intermediate',
+      'J' => 'Intermediate Elite',
+      'S' => 'Standard',
+      'R' => 'Standard Elite',
+      'F' => 'Fullsize',
+      'G' => 'Fullsize Elite',
+      'P' => 'Premium',
+      'U' => 'Premium Elite',
+      'L' => 'Luxury',
+      'W' => 'Luxury Elite',
+      'O' => 'Oversize',
+      'X' => 'Special',
+    }
+
+    TYPE = {
+      'B' => '2-3 Door',
+      'C' => '2/4 Door',
+      'D' => '4-5 Door',
+      'W' => 'Wagon/Estate',
+      'V' => 'Passenger Van',
+      'L' => 'Limousine/Sedan',
+      'S' => 'Sport',
+      'T' => 'Convertible',
+      'F' => 'SUV',
+      'J' => 'Open Air All Terrain',
+      'X' => 'Special',
+      'P' => 'Pick up (single/extended cab) 2 door',
+      'Q' => 'Pick up (double cab) 4 door',
+      'Z' => 'Special Offer Car',
+      'E' => 'Coupe',
+      'M' => 'Monospace',
+      'R' => 'Recreational Vehicle',
+      'H' => 'Motor Home',
+      'Y' => '2 Wheel Vehicle',
+      'N' => 'Roadster',
+      'G' => 'Crossover',
+      'K' => 'Commercial Van/Truck',
+    }
+
+    TRANSMISSION = {
+      'M' => 'Manual Unspecified Drive',
+      'N' => 'Manual 4WD',
+      'C' => 'Manual AWD',
+      'A' => 'Auto Unspecified Drive',
+      'B' => 'Auto 4WD',
+      'D' => 'Auto AWD',
+    }
+
+    FUEL_AC = {
+      'R' => 'Unspecified Fuel/Power With Air',
+      'N' => 'Unspecified Fuel/Power Without Air',
+      'D' => 'Diesel Air',
+      'Q' => 'Diesel No Air',
+      'H' => 'Hybrid Air',
+      'I' => 'Hybrid Plug in Air',
+      'E' => 'Electric (Distance < 250mi/400km) Air',
+      'C' => 'Electric Plus (Distance ≥ 250mi/400km) Air',
+      'L' => 'LPG/Compressed Gas Air',
+      'S' => 'LPG/Compressed Gas No Air',
+      'A' => 'Hydrogen Air',
+      'B' => 'Hydrogen No Air',
+      'M' => 'Multi Fuel/Power Air',
+      'F' => 'Multi fuel/power No Air',
+      'V' => 'Petrol Air',
+      'Z' => 'Petrol No Air',
+      'U' => 'Ethanol Air',
+      'X' => 'Ethanol No Air',
+    }
+  end
 end
+
