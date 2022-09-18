cba8fbf9f728175b86bea4d2f38a3999f75d4b4b
Max Buslaev <max@buslaev.net>
2022-09-18 21:06:51 +0300

    fix typo

--- lib/sipp/inverted.rb
+++ lib/sipp/inverted.rb
@@ -4,12 +4,12 @@ module SIPP
     include InvertedDictionary
 
       def self.generate(capabilities)
-        category = CATEGORY[capabilities[:category]] || '*'
-        type = TYPE[capabilities[:type]] || '*'
-        transmission_dive = TRANSMISSION_DRIVE[[capabilities[:transmission], (capabilities[:drive] || :unspecified)]] || '*'
-        fuel_ac = FUEL_AC[[(capabilities[:fuel] || :unspecified), capabilities[:ac]]] || '*'
+        category = CATEGORY[capabilities[:category]]
+        type = TYPE[capabilities[:type]]
+        transmission_drive = TRANSMISSION_DRIVE[[capabilities[:transmission], (capabilities[:drive] || :unspecified)]]
+        fuel_ac = FUEL_AC[[(capabilities[:fuel] || :unspecified), capabilities[:ac]]]
 
-        [category, type, transmission_dive, fuel_ac].join
+        [category, type, transmission_drive, fuel_ac].map{|cap| cap || '*'}.join
       end
   end
 end
